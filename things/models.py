4cf21fa9810a1b6c6101b587de500db9f701bd76
Stephanie Lawal <stephanie.1.lawal@kcl.ac.uk>
2022-10-06 23:38:25 +0100

    Make basic thing model and make migrations

--- things/models.py
+++ things/models.py
@@ -1,3 +1,8 @@
 from django.db import models
+from django.db.models import Model
 
 # Create your models here.
+class Thing(Model):
+    name = models.CharField(max_length=25)
+    description = models.TextField()
+    quantity = models.IntegerField()
