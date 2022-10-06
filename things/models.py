b41f02c1d89a7fd1311e1da58a84cb119baf1e8c
Stephanie Lawal <stephanie.1.lawal@kcl.ac.uk>
2022-10-07 00:12:28 +0100

    Refine Thing model

--- things/models.py
+++ things/models.py
@@ -3,6 +3,6 @@ from django.db.models import Model
 
 # Create your models here.
 class Thing(Model):
-    name = models.CharField(max_length=25)
-    description = models.TextField()
-    quantity = models.IntegerField()
+    name = models.CharField(max_length=30, unique=True, blank=False)
+    description = models.TextField(max_length=120)
+    quantity = models.IntegerField(validators=[MinValueValidator(0), MaxValueValidator(100)])
