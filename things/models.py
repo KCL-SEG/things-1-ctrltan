43682f7a9d7075e9bdde54db69d34040654bc065
Stephanie Lawal <stephanie.1.lawal@kcl.ac.uk>
2022-10-07 00:15:47 +0100

    Refine Thing model and import validators

--- things/models.py
+++ things/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 from django.db.models import Model
+from django.core.validators import MinValueValidator, MaxValueValidator
 
 # Create your models here.
 class Thing(Model):
